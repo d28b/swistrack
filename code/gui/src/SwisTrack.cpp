fcea9e8d7645f4f76fed94f5de05ea01cfc4663d
nikolauscorrell <nikolauscorrell@18c3c8b4-d610-0410-8eed-94a590f67c64>
2006-09-18 23:21:36 +0000

    -documentation added
    git-svn-id: svn+ssh://svn.code.sf.net/p/swistrack/code@140 18c3c8b4-d610-0410-8eed-94a590f67c64

--- code/gui/src/SwisTrack.cpp
+++ code/gui/src/SwisTrack.cpp
@@ -1,3 +1,15 @@
+/*! \mainpage My Personal Index Page
+*
+* \section intro_sec Introduction
+*
+* This is the introduction.
+*
+* \section install_sec Installation
+*
+* \subsection step1 Step 1: Opening the box
+*
+* etc...
+*/
 #include "SwisTrack.h"
 #include "Canvas.h"
 //#include "DataLogger.h"
