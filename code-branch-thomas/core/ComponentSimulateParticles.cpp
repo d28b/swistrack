62bfcd785241c5a71a5931375cc7d1802bc3c7ee
lochmatter <lochmatter@18c3c8b4-d610-0410-8eed-94a590f67c64>
2007-01-26 14:14:03 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/swistrack/code@247 18c3c8b4-d610-0410-8eed-94a590f67c64

---
+++ code-branch-thomas/core/ComponentSimulateParticles.cpp
@@ -0,0 +1,61 @@
+#include "ComponentSimulateParticles.h"
+#define THISCLASS ComponentSimulateParticles
+
+THISCLASS::ComponentSimulateParticles(SwisTrackCore *stc):
+		Component(stc, "ParticleFilter"),
+		mMinArea(0), mMaxArea(1000000), mMaxNumber(10), mFirstDilate(1), mFirstErode(1), mSecondDilate(1) {
+
+	// Data structure relations
+	mDisplayName="Blob detection with min/max particle size";
+	mCategory=&(mCore->mCategoryBlobDetection);
+	AddDataStructureWrite(&(mCore->mDataStructureParticles));
+}
+
+THISCLASS::~ComponentSimulateParticles() {
+}
+
+void THISCLASS::OnStart() {
+	OnReloadConfiguration();
+	mNumParticles=GetConfigurationDouble("NumParticles", 10);	
+
+	// Check for stupid configurations
+	if (mNumParticles<1) {
+		AddError("You must choose at least one particle.");
+	}
+
+	// Create the robots at their initial position
+	
+	mParticles
+}
+
+void THISCLASS::OnReloadConfiguration() {
+	mCameraOrigin.x=GetConfigurationDouble("CameraOrigin.x", 0);
+	mCameraOrigin.y=GetConfigurationDouble("CameraOrigin.y", 0);
+	mCameraRotation=GetConfigurationDouble("CameraRotation", 0);
+	mCameraPixelSize=GetConfigurationDouble("CameraPixelSize", 1);
+	mNoise=GetConfigurationDouble("Noise", 0);
+
+	// Check for stupid configurations
+	if (mCameraPixelSize<0) {
+		AddError("The pixel size must be bigger than 0.");
+	}
+}
+
+void THISCLASS::OnStep() {
+	
+
+	// Set these particles
+	mCore->mDataStructureParticles.mParticles=&mParticles;
+}
+
+void THISCLASS::OnStepCleanup() {
+	mCore->mDataStructureParticles.mParticles=0;
+}
+
+void THISCLASS::OnStop() {
+}
+
+double THISCLASS::GetContourCompactness(const void* contour) {
+	double l = cvArcLength(contour, CV_WHOLE_SEQ, 1);
+	return fabs(12.56*cvContourArea(contour)/(l*l));	
+}
---
+++ code-branch-thomas/core/ComponentSimulateParticles.h
@@ -0,0 +1,36 @@
+#ifndef HEADER_ComponentSimulateParticles
+#define HEADER_ComponentSimulateParticles
+
+#include "cv.h"
+#include "Component.h"
+#include "DataStructureParticles.h"
+#include <vector>
+
+//! Simulates particles
+class ComponentSimulateParticles: public Component {
+
+public:
+	ComponentSimulateParticles(SwisTrackCore *stc);
+	~ComponentSimulateParticles();
+
+	// Overwritten Component methods
+	void OnStart();
+	void OnReloadConfiguration();
+	void OnStep();
+	void OnStepCleanup();
+	void OnStop();
+	Component *Create() {return new ComponentSimulateParticles(mCore);}
+
+private:
+	CvSize mCameraOrigin;
+	double mCameraRotation;
+	double mCameraPixelSize;
+	double mNoise;
+	int mFile;
+
+	std::vector<tRobot> mRobots;
+	DataStructureParticles::tParticleVector mParticles;
+};
+
+#endif
+
