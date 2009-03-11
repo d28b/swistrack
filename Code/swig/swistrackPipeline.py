07f25ff709b89d1af12427e90ca21a5f694dadfd
stefie10 <stefie10@18c3c8b4-d610-0410-8eed-94a590f67c64>
2009-03-11 15:44:42 +0000

    Do absolute path conversion inside the component.
    git-svn-id: svn+ssh://svn.code.sf.net/p/swistrack/code@739 18c3c8b4-d610-0410-8eed-94a590f67c64

--- Code/swig/swistrackPipeline.py
+++ Code/swig/swistrackPipeline.py
@@ -1,28 +1,30 @@
 import swistrack
+from os.path import abspath
 class SwistrackError(Exception):
     def __init__(self, msg):
-        Error.__init__(self, msg)
+        Exception.__init__(self, msg)
 class Pipeline:
     def __init__(self, componentsDirectory, pipelineFile):
-        self.core = swistrack.SwisTrackCore(componentsDirectory)
+        self.pipelineFile = abspath(pipelineFile)
+        self.core = swistrack.SwisTrackCore(abspath(componentsDirectory))
         cr = swistrack.ConfigurationReaderXML()
-        if not cr.Open("backgroundSubtraction.swistrack"):
-            raise SwistrackException("Coudln't read file: %s" % pipelineFile)
+        if not cr.Open(self.pipelineFile):
+            raise SwistrackError("Coudln't read file: %s" % self.pipelineFile)
 
 	cr.ReadComponents(self.core)
         for x in cr.mErrorList.mList:
             print "x", x.mMessage
         if len(cr.mErrorList.mList) != 0:
-            raise SwistrackException("Errors in file: %s" % pipelineFile)
+            raise SwistrackError("Errors in file: %s" % self.pipelineFile)
         self.core.TriggerStart();
 	self.core.Start(False)
         if self.printErrors():
-            raise SwistrackException("Errors in file: %s" % pipelineFile)
+            raise SwistrackError("Errors in file: %s" % self.pipelineFile)
 
     def step(self):
         self.core.Step()
         if self.printErrors():
-            raise SwistrackException("Errors in step.")
+            raise SwistrackError("Errors in step.")
     
 
      
