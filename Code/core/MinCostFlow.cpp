c7d637b57e134bc9a6902928a0526437c85ef3a1
stefie10 <stefie10@18c3c8b4-d610-0410-8eed-94a590f67c64>
2009-03-16 05:53:19 +0000

    Successfully calling max flow with boost.  Whew!
    git-svn-id: svn+ssh://svn.code.sf.net/p/swistrack/code@748 18c3c8b4-d610-0410-8eed-94a590f67c64

--- Code/core/MinCostFlow.cpp
+++ Code/core/MinCostFlow.cpp
@@ -14,57 +14,67 @@ void testFlow()
   typedef MinCostFlow::MinCostFlowGraph::vertex_descriptor vertex_descriptor;
   
   vProps.name = "1";
+  vProps.net_supply = 5;
   vertex_descriptor v1 = add_vertex(vProps, graph);
 
   vProps.name = "2";
+  vProps.net_supply = 2;
   vertex_descriptor v2 = add_vertex(vProps, graph);
 
   vProps.name = "3";
+  vProps.net_supply = 0;
   vertex_descriptor v3 = add_vertex(vProps, graph);
 
   vProps.name = "4";
+  vProps.net_supply = -2;
   vertex_descriptor v4 = add_vertex(vProps, graph);
 
   vProps.name = "5";
+  vProps.net_supply = -4;
   vertex_descriptor v5 = add_vertex(vProps, graph);
 
-  // outgoing 1
-  eProps.cost = 5;
-  eProps.capacity = 11;
-  add_edge(v1, v2, eProps, graph);
+  vProps.name = "6";
+  vProps.net_supply = -1;
+  vertex_descriptor v6 = add_vertex(vProps, graph);
 
-  eProps.cost = 4;
-  eProps.capacity = 16;
+  // outgoing 1
+  eProps.cost = 3;
+  eProps.capacity = 4;
   add_edge(v1, v3, eProps, graph);
+
+  eProps.cost = 3;
+  eProps.capacity = 1;
+  add_edge(v1, v2, eProps, graph);
   
   // outgoing 2
-  eProps.cost = 4;
-  eProps.capacity = 18;
-  add_edge(v2, v4, eProps, graph);
+  eProps.cost = 7;
+  eProps.capacity = 2;
+  add_edge(v2, v3, eProps, graph);
 
   // outgoing 3
-  eProps.cost = 3;
-  eProps.capacity = 13;
+  eProps.cost = 1;
+  eProps.capacity = 8;
+  add_edge(v3, v6, eProps, graph);
+
+  eProps.cost = 7;
+  eProps.capacity = 5;
+  add_edge(v3, v5, eProps, graph);
+
+  eProps.cost = 5;
+  eProps.capacity = 2;
   add_edge(v3, v4, eProps, graph);
 
-  // no outgoing 4
-
-  // outgoing 5
+  // outgoing 4
   eProps.cost = 3;
-  eProps.capacity = 10;
-  add_edge(v5, v2, eProps, graph);
+  eProps.capacity = 1;
+  add_edge(v4, v5, eProps, graph);
 
-  eProps.cost = 5;
-  eProps.capacity = 11;
-  add_edge(v5, v1, eProps, graph);
+  // no outgoing 5
 
-  eProps.cost = 5;
-  eProps.capacity = -25;
-  add_edge(v5, v3, eProps, graph);
-
+  // no outgoing 6
 
   
-  flow.minCostFlow(graph);
+  flow.minCostFlow(graph, v1, v4);
 
 }
 
--- Code/core/MinCostFlow.h
+++ Code/core/MinCostFlow.h
@@ -19,21 +19,56 @@ void testFlow();
 
 class MinCostFlow {
  public:
+  struct VertexProps;
+  struct EdgeProps;
+  typedef adjacency_list < listS, vecS, bidirectionalS, 
+    VertexProps,
+    EdgeProps > MinCostFlowGraph;
+
   struct VertexProps {
     std::string name;
+    int net_supply;
   };
   struct EdgeProps {
     double cost;
     int capacity;
+    int residual_capacity;
+    MinCostFlowGraph::edge_descriptor reverse;
+    // flow is capacity - residual_capacity
   };
-  typedef adjacency_list < listS, vecS, directedS, 
-    VertexProps,
-    EdgeProps > MinCostFlowGraph;
 
-  void minCostFlow(MinCostFlowGraph graph) {
+
+  void minCostFlow(MinCostFlowGraph graph, 
+		   MinCostFlowGraph::vertex_descriptor sourceVertex,
+		   MinCostFlowGraph::vertex_descriptor sinkVertex) {
     cout << "Computing min cost flow." << endl;
 
+    graph_traits < MinCostFlowGraph >::edge_iterator ei, ei_end;
     MinCostFlowGraph::vertex_descriptor v  = * vertices(graph).first;
+    typedef  pair < MinCostFlowGraph::vertex_descriptor,
+      MinCostFlowGraph::vertex_descriptor > EdgePair;
+    vector<MinCostFlowGraph::edge_descriptor> cachedEdges;
+      
+    for (tie(ei, ei_end) = edges(graph); ei != ei_end; ++ei) {
+      cachedEdges.push_back(*ei);
+    }
+
+    struct MinCostFlow::EdgeProps eProps;
+    eProps.capacity = 0;
+    eProps.cost = 0;
+    eProps.residual_capacity = 0;
+    
+    for (vector<MinCostFlowGraph::edge_descriptor>::iterator i = 
+	   cachedEdges.begin();
+	 i != cachedEdges.end(); i++) {
+      
+      MinCostFlowGraph::vertex_descriptor s = source(*i, graph);
+      MinCostFlowGraph::vertex_descriptor t = target(*i, graph);
+      eProps.reverse = *i;
+      pair<MinCostFlowGraph::edge_descriptor, bool> newEdge 
+	= add_edge(t, s, eProps, graph);
+      graph[*i].reverse = newEdge.first;
+    }
     
     
     vector<double> distances(num_vertices(graph));
@@ -53,6 +88,24 @@ class MinCostFlow {
       }
     } else {
       cout << "Negative cycle." << endl;
+    }
+
+    property_map<MinCostFlowGraph, edge_reverse_t>::type 
+      rev = get(edge_reverse, graph);
+
+    
+    long flow = push_relabel_max_flow
+      (graph, sourceVertex, sinkVertex,
+       capacity_map(get(&EdgeProps::capacity, graph)).
+       residual_capacity_map(get(&EdgeProps::residual_capacity, graph)).
+       vertex_index_map(identity_property_map()).
+       reverse_edge_map(get(&EdgeProps::reverse, graph)));
+    cout << "Flow: " << flow << endl;
+    
+    for (tie(ei, ei_end) = edges(graph); ei != ei_end; ++ei) {
+      cout << "Flow in node: " << graph[source(*ei, graph)].name << 
+	" to " << graph[target(*ei, graph)].name << ": ";
+      cout << (graph[*ei].capacity - graph[*ei].residual_capacity) << endl;
     }
 
 
