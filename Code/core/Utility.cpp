8398287cc2f20431c3904bd68a9b1dc537411b5b
stefie10 <stefie10@18c3c8b4-d610-0410-8eed-94a590f67c64>
2009-06-03 13:46:21 +0000

    Commented out, undebugged function to convert a rectangle to a box.
    git-svn-id: svn+ssh://svn.code.sf.net/p/swistrack/code@897 18c3c8b4-d610-0410-8eed-94a590f67c64

--- Code/core/Utility.cpp
+++ Code/core/Utility.cpp
@@ -12,3 +12,15 @@ CvRect THISCLASS::RectByCenter(int centerX, int centerY, int width, int height) 
 	int lowerLefty = centerY - width / 2;
 	return cvRect(lowerLeftx, lowerLefty, width, height);
 }
+
+
+/*CvBox2D THISCLASS::RectToBox(CvRect rect) {
+
+  CvBox2D out;
+  out.center = cvPoint2D32f(rect.x + width / 2.0,
+			    rect.y + height / 2.0);
+  out.size = cvPoint2D32f(rect.width, rect.height);
+  out.angle = 0;
+  return out;
+}
+*/
